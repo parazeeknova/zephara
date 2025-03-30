dc1e6fb698ebf3fdd89584f3f23f31be0afeaf7b
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-03-30 11:16:10 +0530

    feat[TOOLBAR]: Added toolbar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,13 @@
-interface WorkspaceIdPageProps {
-  params: { workspaceId: string };
-}
+'use client';
 
-const WorkspaceIdPage = async ({ params }: WorkspaceIdPageProps) => {
-  const { workspaceId } = await params;
-  return (
-    <div>
-      <h1>WorkspaceIdPage</h1>
-      <p>Workspace ID: {workspaceId}</p>
-    </div>
-  );
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
+  return <div>Data: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
